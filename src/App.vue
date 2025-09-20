a84172c3367e044400fc83673abffbab0facb62c
呆唯 <139741877+agtypec@users.noreply.github.com>
2025-09-20 22:17:21 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,10 @@
 // 不需要额外的 import
 </script>
 
+<!-- 文件: src/App.vue -->
 <template>
-  <!-- 使用 v-slot 获取当前路由的组件 -->
   <router-view v-slot="{ Component }">
-    <!-- 使用 keep-alive 包裹，并指定只缓存 MusicPlayer 组件 -->
-    <keep-alive include="MusicPlayer">
-      <!-- 动态渲染当前路由的组件 -->
+    <keep-alive include="HomeView">
       <component :is="Component" />
     </keep-alive>
   </router-view>
