541c7a60665b06b647d0ae6583d064894003cf80
呆唯 <139741877+agtypec@users.noreply.github.com>
2025-09-20 21:39:55 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,17 @@
 <script setup>
-
-import { RouterView } from 'vue-router';
-
-
+// 不需要额外的 import
 </script>
 
 <template>
-    <RouterView></RouterView>
+  <!-- 使用 v-slot 获取当前路由的组件 -->
+  <router-view v-slot="{ Component }">
+    <!-- 使用 keep-alive 包裹，并指定只缓存 MusicPlayer 组件 -->
+    <keep-alive include="MusicPlayer">
+      <!-- 动态渲染当前路由的组件 -->
+      <component :is="Component" />
+    </keep-alive>
+  </router-view>
 </template>
 
 <style scoped>
-
 </style>
